4774d84a1b94e4c999a28371a1df0dd47e8f9b24
Akhtar Hossen <akhtarprog@gmail.com>
2024-07-27 12:24:56 +0600

    some

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ let express = require('express')
 let app = express()
 let userRouter = require('./routers/users.routers')
 let adminRouter = require('./routers/admin.routers')
+let bodyPerser = require('body-parser')
+
+app.use(bodyPerser.urlencoded({encoded:false}))
+app.use(bodyPerser.json())
 
 app.use('/user', userRouter)
 app.use('/admin', adminRouter)
 
+
+
 app.use('/', (req,res)=>{
-    res.send("<h1>This is a defult Home page</h1>")
-    res.end()
+    res.sendFile(__dirname+"/index.html")
+    res.statusCode = 200
 })
+
+
 
 app.listen(3000, ()=>{
     console.log('http://localhost:3000');
--- routers/users.routers.js
+++ routers/users.routers.js
@@ -3,11 +3,17 @@ let router = express.Router()
 
 
 router.get('/login', (req, res)=>{
-    res.send('this is login page')
+    res.send('this is your login page')
 })
 
 router.get('/registar', (req, res)=>{
-    res.send('this is registar page')
+    res.sendFile(__dirname + "/user.html")
+})
+
+router.post('/registar', (req, res)=>{
+    let fullName = req.body.fullName;
+    let age = req.body.age;
+    res.send(`Your full name ${fullName} and your age ${age}`)
 })
 
 
